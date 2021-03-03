2132f14f9ad80ad2756d6e4d9c1dc48b3e6b0cf1
Lucas Serain <lucas.serain@outlook.com>
2021-03-02 21:51:13 -0300

    :poop: Página de login provisória - André irá adicionar

--- src/pages/Sigin/index.tsx
+++ src/pages/Sigin/index.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo_small.png';
 
-const SignIn: React.FC = () => <h1>SignIn</h1>;
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="Parfacemotion" />
+
+      <form action="">
+        <h1>Faça o seu login</h1>
+
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
 
 export default SignIn;
---
+++ src/pages/Sigin/styles.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+import signInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #ffffff;
+
+      & + input {
+        margin-top: 8px;
+      }
+
+      &::placeholder {
+        color: #ffffff;
+      }
+    }
+
+    button {
+      background: #07e8fc;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+      &:hover {
+        background: ${shade(0.2, '#07e8fc')};
+      }
+    }
+    a {
+      color: #ffffff;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+      &:hover {
+        color: ${shade(0.2, '#ffffff')};
+      }
+    }
+  }
+
+  > a {
+    color: #07e8fc;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    margin-right: 16px;
+  }
+  &:hover {
+    color: ${shade(0.2, '#07e8fc')};
+  }
+`;
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
