6d57638402c992569fdca6a43e8329716d928c62
Lucas Serain <36819311+lucasserain@users.noreply.github.com>
2021-06-16 19:41:44 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,9 @@ import SignIn from '../pages/SignIn/index';
 import SignUp from '../pages/SignUP/index';
 import Dashboard from '../pages/Dashboard/index';
 import Route from './routes';
+import App from '../pages/Recorder/App.js';
 import Report from '../pages/Report';
+import Record from '../pages/Record';
 import CadastroDisciplina from '../pages/CadastroDisciplinas';
 import Disciplina from '../pages/Disciplina';
 import ListaDisciplina from '../pages/ListaDiscplinas';
@@ -14,11 +16,13 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={App} />
+    <Route path="/report" component={Report} isPrivate />
+    <Route path="/report/:coddisciplina+" component={Report} isPrivate />
     <Route path="/record/:aula+" component={Dashboard} isPrivate />
     <Route path="/caddisciplinas" component={CadastroDisciplina} isPrivate />
     <Route path="/disciplinas/:disciplina+" component={Disciplina} isPrivate />
     <Route path="/listadisciplinas" component={ListaDisciplina} isPrivate />
-    <Route path="/report/:coddisciplina+" component={Report} isPrivate />
   </Switch>
 );
 
